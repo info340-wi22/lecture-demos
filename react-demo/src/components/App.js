4792bed826cbc2c1a788c54fafd73b05e9f687eb
Joel Ross <joelross@uw.edu>
2022-03-01 10:15:29 -0800

    End of lecture code

--- react-demo/src/components/App.js
+++ react-demo/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 //example GitHub repo data
 const EXAMPLE_DATA = [
@@ -9,42 +9,91 @@ const EXAMPLE_DATA = [
 
 
 function App(props) {
-  const [data, setData] = useState(EXAMPLE_DATA);
+  const [data, setData] = useState([]);
   //control form
   const [queryInput, setQueryInput] = useState('');
+
+  const initialSearch = props.initialSearch;
+
+  useEffect(() => {
+    //function: what to do when the app FIRST loads
+
+    //all together now
+    //fetch("/chat_log.json") //root is the public folder
+    fetch("https://api.github.com/search/repositories?q="+initialSearch)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log("recieved:", data);
+            setData(data.items);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+
+    function cleanup() {
+      console.log("component is being removed")
+      console.log("turn off the lights")
+    }
+    return cleanup;
+  }, [])
+
 
   const handleChange = (event) => {
     setQueryInput(event.target.value);
   }
 
   const handleSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault(); //don't send normal form data
 
+    //all together now
+    fetch("https://api.github.com/search/repositories?q="+queryInput)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log("recieved:", data);
+            setData(data.items);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
   }
 
   //render the data
+  console.log("rendering with data", data);
+
   const dataElemArray = data.map((repo) => {
-    return <li><a href={repo.html_url}>{repo.name}</a></li>
+    return <li key={repo.html_url}><a href={repo.html_url}>{repo.full_name}</a></li>
   })
 
   return (
     <div className="container">
       <header><h1>AJAX Demo</h1></header> 
 
-      <form method="GET" action="https://api.github.com/search/repositories">
+
+
+      <form method="GET" action="https://api.github.com/search/repositories" onSubmit={handleSubmit}>
         <input type="text" className="form-control mb-2" 
           name="q"
           placeholder="Search Github for..."
           value={queryInput} onChange={handleChange}
         />
-        <button type ="submit" className="btn btn-primary">Search!</button>
+        <input type="hidden" name="sort" value="best_match"></input>
+        <button type="submit" className="btn btn-primary">Search!</button>
       </form>
 
-      <div className="mt-4">
-        <h2>Results</h2>
-        {/* results go here */}
-        {dataElemArray}
-      </div>
+
+      {data.length === 0 && <p>No results yet</p>}
+      {data.length !== 0 &&
+
+        <div className="mt-4">
+          <h2>Results</h2>
+          {/* results go here */}
+          {dataElemArray}
+        </div>
+      }
     </div>
   )
 }
--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,10 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//make sure that fetch is globally available no matter which browser
+import 'whatwg-fetch'; 
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'; //include css
 
 import App from './components/App';
 
+
+// //for demo purpose only!!
+// // console.log("about to send")
+// const fetchPromise = fetch("https://api.github.com/search/repositories?q=d3")
+
+// //addEventListener('data-ready-event', whattodowhenfulfilled)
+// const thatDataPromise = fetchPromise.then((response) => {
+//     console.log("promise fulfilled");
+//     //console.log(response); //http response from the server
+//     // if(response.status == 200) //looks okay
+//     console.log("about to open letter")
+//     const dataPromise = response.json(); //extract the body into a JSON object
+//     console.log(dataPromise);
+//     console.log("started opening letter")
+
+//     return dataPromise;
+// }) //give instructions what to do when ready
+
+// thatDataPromise.then((data) => {
+//     console.log("data promise response:")
+//     console.log(data);
+// })
+
+// console.log("request sent!")
+
+// console.log("request sent!")
+
+
+// const fetchData = async () => {
+//     let data = {}
+//     try {
+//         const response = await fetch("https://api.github.com/search/repositories?q=d3")
+//         data = await response.json()
+//     }
+//     catch(error) {
+//         console.log(error);
+//     }
+
+//     console.log(data);
+// }
+// fetchData();
+// console.log("runs immediatekly");
+
 //show the content in the web page (inside #root)
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App initialSearch="react" />, document.getElementById('root'));
